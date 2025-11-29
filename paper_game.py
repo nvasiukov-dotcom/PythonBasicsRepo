5e95bc222f25ba5d2e121744ba90e1fca1c802b3
Nikita Vasiukov <nvasiukov@gmail.com>
2025-11-29 19:55:32 +0300

    Домашнее задание простой вариант игры Камень Ножницы Бумага

--- paper_game.py
+++ paper_game.py
@@ -1,1 +1,25 @@
-print("Hello, GitHub!")
+from random import choice
+User: str = input("Выберите один из вариантов, Камень или Ножницы или Бумага: ")
+Comp: str = choice(["Камень", "Ножницы", "Бумага"])
+print("Выбор игрока: ",(User))
+print("Выбор компютера: ",(Comp))
+if User == Comp:
+    print("Ничья")
+elif User == "Камень":
+    if Comp == "Ножницы":
+        print("Вы победили")
+    else:
+        print("Вы проиграли")
+elif User == "Ножницы":
+    if Comp == "Бумага":
+        print("Вы победили")
+    else:
+        print("Вы проиграли")
+elif User == "Бумага":
+    if Comp == "Камень":
+         print("Вы победили")
+    else:
+        print("Вы проиграли")
+
+            
+            
